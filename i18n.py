adbedaa0f60a0cbbfa58d6d0baae9f836a641c0c
Saiyd <koringajpgames@gmail.com>
2023-07-10 00:57:00 -0300

    Update i18n.py

--- i18n.py
+++ i18n.py
@@ -16,8 +16,8 @@ class I18nAuto:
                 0
             ]  # getlocale can't identify the system's language ((None, None))
         if not os.path.exists(f"./i18n/{language}.json"):
-            language = "es_ES"
-        language = "es_ES"
+            language = "pt_BR"
+        language = "pt_BR"
         self.language = language
         # print("Use Language:", language)
         self.language_map = load_language_list(language)
